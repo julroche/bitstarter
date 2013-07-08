b6c76449ce8fb5d6af413a2696436e541ad34d53
Julie Roche <julroche@yahoo.com>
2013-07-08 19:45:48 +0000

    Added var buf to web.js

--- web.js
+++ web.js
@@ -2,10 +2,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var msg = fs.readFile('/index.html', 'utf8');
+var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send(msg);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
